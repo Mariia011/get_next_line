398e16258d3bad377f2549e19084a2ca5774ed69
Mariia Khachaturova <marikhac@c2r6s3.42yerevan.am>
2024-02-13 18:48:00 +0400

    no changes

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marikhac <marikhac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 12:34:10 by marikhac          #+#    #+#             */
+/*   Updated: 2024/02/13 16:43:40 by marikhac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 24
+# endif
+
+char	*get_next_line(int fd);
+
+
+# endif
